8b9a0fcf7cce927fc81964672fb87c5e26d16f30
sabsab <zilzilya@gmail.com>
2013-06-06 14:15:43 +0400

    composer support

--- ExcelTemplateFacade.php
+++ ExcelTemplateFacade.php
@@ -7,8 +7,17 @@
  * @date: 06.03.13
  * @version    $Id: $
  */
-require_once 'lib/PHPExcel/Classes/PHPExcel.php';
-require_once 'lib/PHPReport/PHPReport.php';
+define ("USE_COMPOSER", TRUE);
+
+if (USE_COMPOSER == TRUE ){
+    require_once 'library/autoload.php';
+}
+else {
+    require_once 'lib/PHPExcel/Classes/PHPExcel.php';
+    require_once 'lib/PHPReport/PHPReport.php';
+}
+
+
 
 define ("_MPDF_CONFIG_PATH", dirname(preg_replace('/\\\\/','/',__FILE__)) . '/config/mpdf/');
 define ("_MPDF_TTFONTPATH", dirname(preg_replace('/\\\\/','/',__FILE__)) . '/config/mpdf/ttfonts/');
@@ -47,15 +56,22 @@ class ExcelTemplateFacade {
     public function getTemplater(){
         if (NULL == $this->templater){
 
+            // @todo refactoring binding libs
+            if (USE_COMPOSER){
+                $path_mpdf = "/library/sabsab/mpdf/";
+            }
+            else {
+                $path_mpdf = '/lib/mpdf/';
+            }
             PHPExcel_Settings::setPdfRenderer(
                 PHPExcel_Settings::PDF_RENDERER_MPDF,
-                realpath(dirname(__FILE__).'/lib/mpdf/')
+                realpath(dirname(__FILE__).$path_mpdf)
             );
             PHPExcel_Settings::setLocale('ru');
             $templater = new PHPReport(
                 array(
                     'templatePath'=>dirname($this->getAbsTemplatePath()) . "/",
-                    'pdfRendererPath'=>"/lib/mpdf/",
+                    'pdfRendererPath'=>$path_mpdf,
                     'template'=>basename($this->getAbsTemplatePath())
                 )
             );
